4a7d26ac56d0cecafcdcea2b6ca6ee35a503525e
hbourget <hugobourget@protonmail.com>
2021-11-26 08:10:10 +0100

    minor fix

--- sources/config.c
+++ sources/config.c
@@ -10,7 +10,7 @@ ServerConfig initCfg()
   config_init(&cfg);
 
   /* Read the file. If there is an error, report it and exit. */
-  if(! config_read_file(&cfg, "../config.cfg"))
+  if(! config_read_file(&cfg, "../server.config"))
   {
     fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg),config_error_line(&cfg), config_error_text(&cfg));
     config_destroy(&cfg);
